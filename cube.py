714d8634013fff61f960ee7589651de44cb746e6
solivero <oliver.ce.petri@gmail.com>
2015-07-07 01:14:12 +0200

    All required rotations implemented

--- cube.py
+++ cube.py
@@ -1,25 +1,10 @@
-from enum import Enum
-from copy import deepcopy
 from math import sin, cos, radians
-class Face(Enum):
-    up = 1
-    down = 2
-    front = 3
-    back = 4
-    left = 5
-    right = 6
 
-class Color(Enum):
-    white = 1
-    yellow = 2
-    blue = 3
-    green = 4
-    red = 5
-    orange = 6
+from enums import Color, Face
 
 class Cublet:
     def __repr__(self):
-        return "x:{} y:{} z:{} colors:{}\n".format(self.x, self.y, self.z, self.stickers)
+        return "x:{} y:{} z:{} \ncolors:{}\n\n".format(self.x, self.y, self.z, self.stickers)
 
     def __init__(self, x, y, z, stickers):
         self.x = x
@@ -29,23 +14,121 @@ class Cublet:
 
 class Cube:
     def __repr__(self):
-        return self.cublets
+        return repr(self.cublets)
 
-    def rotate_cube(self, theta):
-        theta = radians(theta)
-        sin_t = sin(theta)
-        cos_t = cos(theta)
-        if theta == 90:
-            sin_t = int(sin_t)
-            cos_t = int(cos_t)
+    def rotate_cublet_x(self, cublet):
+        ### 90 deg == counter clockwise
+        ### not needed as sin(90)=1 and cos(90)=0
+        ### theta = radians(theta)
+        ### sin_t = sin(theta)
+        ### cos_t = cos(theta)
+        face_convert = {
+            Face.front: Face.up,
+            Face.right: Face.right,
+            Face.back: Face.down,
+            Face.left: Face.left,
+            Face.down: Face.front,
+            Face.up: Face.back,
+        }
+        z = cublet.z
+        y = cublet.y
+        cublet.z = -y
+        cublet.y = z
+        for sticker in cublet.stickers:
+            sticker['face'] = face_convert[sticker['face']]
+        return cublet
 
-        for row in self.cublets:
-            for depth in row:
+    def rotate_cublet_y(self, cublet):
+        face_convert = {
+            Face.front: Face.front,
+            Face.right: Face.up,
+            Face.back: Face.back,
+            Face.left: Face.down,
+            Face.down: Face.right,
+            Face.up: Face.left,
+        }
+        x = cublet.x
+        z = cublet.z
+        cublet.x = -z
+        cublet.z = x
+        for sticker in cublet.stickers:
+            sticker['face'] = face_convert[sticker['face']]
+        return cublet
+
+    def rotate_cublet_z(self, cublet):
+        face_convert = {
+            Face.front: Face.right,
+            Face.right: Face.back,
+            Face.back: Face.left,
+            Face.left: Face.front,
+            Face.down: Face.down,
+            Face.up: Face.up,
+        }
+        x = cublet.x
+        y = cublet.y
+        cublet.x = -y
+        cublet.y = x
+        for sticker in cublet.stickers:
+            sticker['face'] = face_convert[sticker['face']]
+        return cublet
+
+    def rotate_face(self, face, clockwise):
+        if clockwise:
+            p = 3
+            q = 1
+        else:
+            p = 1
+            q = 3
+
+        if face is Face.up:
+            for i in range(p):
+                for column in self.cublets:
+                    for depth in column:
+                        depth[2] = self.rotate_cublet_z(depth[2])
+        elif face is Face.left:
+            for i in range(p):
+                for depth in self.cublets[0]:
+                    for cublet in depth:
+                        cublet = self.rotate_cublet_x(cublet)
+        elif face is Face.front:
+            for i in range(p):
+                for column in self.cublets:
+                    for cublet in column[0]:
+                        cublet = self.rotate_cublet_y(cublet)
+        elif face is Face.down:
+            for i in range(q):
+                for column in self.cublets:
+                    for depth in column:
+                        depth[0] = self.rotate_cublet_z(depth[0])
+
+        elif face is Face.right:
+            for i in range(q):
+                for depth in self.cublets[2]:
+                    for cublet in depth:
+                        cublet = self.rotate_cublet_x(cublet)
+        elif face is Face.back:
+            for i in range(q):
+                for column in self.cublets:
+                    for cublet in column[0]:
+                        cublet = self.rotate_cublet_y(cublet)
+
+    def rotate_cube(self, clockwise):
+        if clockwise:
+            n = 3
+        else:
+            n = 1
+        for column in self.cublets:
+            for depth in column:
                 for cublet in depth:
-                    x = cublet.x
-                    y = cublet.y
-                    cublet.x = x * cos_t - y * sin_t
-                    cublet.y = y * cos_t + x * sin_t
+                    cublet = self.rotate_cublet_z(cublet)
+
+    def flip_cube(self):
+        for i in range(2):
+            for column in self.cublets:
+                for depth in column:
+                    for cublet in depth:
+                        cublet = self.rotate_cublet_x(cublet)
+
     def __init__(self):
 
     ### X(left(red)-right(orange)) Y(front(blue)-back(green)) Z (down(yellow)-up(white))
@@ -74,14 +157,14 @@ class Cube:
                     for letter in self.cublets[x][y][z]:
                         for color in Color:
                             if color.name[0] == letter:
-                                stickers.append((color, Face(color.value)))
+                                stickers.append({'color': color, 'face': Face(color.value)})
                                 break
-                    self.cublets[x][y][z] = (Cublet(x-1, y-1, z-1, stickers))
+                    self.cublets[x][y][z] = Cublet(x-1, y-1, z-1, stickers)
 
 
 if __name__ == '__main__':
     cube = Cube()
     print(cube.cublets)
-    cube.rotate_cube(90)
+    cube.rotate_face(Face.up, clockwise=True)
     print("ROTATED")
-    print(cube.cublets)
+    print(cube)
---
+++ enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+class Face(Enum):
+    up = 1
+    down = 2
+    front = 3
+    back = 4
+    left = 5
+    right = 6
+
+class Color(Enum):
+    white = 1
+    yellow = 2
+    blue = 3
+    green = 4
+    red = 5
+    orange = 6
