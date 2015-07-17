a51f3ac0a2b360c60a663f02de2f41bba2371111
solivero <oliver.ce.petri@gmail.com>
2015-07-17 23:36:13 +0200

    Directional point/vector for faces, compatible with trigonomic rotation

--- cube.py
+++ cube.py
@@ -2,9 +2,15 @@ from math import sin, cos, radians
 
 from enums import Color, Face
 
+
 class Cublet:
     def __repr__(self):
-        return "x:{} y:{} z:{} \ncolors:{}\n\n".format(self.x, self.y, self.z, self.stickers)
+        return "x:{} y:{} z:{} \ncolors:{}\n\n".format(
+            self.x,
+            self.y,
+            self.z,
+            self.stickers
+        )
 
     def __init__(self, x, y, z, stickers):
         self.x = x
@@ -12,64 +18,47 @@ class Cublet:
         self.z = z
         self.stickers = stickers
 
+
 class Cube:
     def __repr__(self):
         return repr(self.cublets)
 
     def rotate_cublet_x(self, cublet):
-        ### 90 deg == counter clockwise
-        ### not needed as sin(90)=1 and cos(90)=0
-        ### theta = radians(theta)
-        ### sin_t = sin(theta)
-        ### cos_t = cos(theta)
-        face_convert = {
-            Face.front: Face.up,
-            Face.right: Face.right,
-            Face.back: Face.down,
-            Face.left: Face.left,
-            Face.down: Face.front,
-            Face.up: Face.back,
-        }
+        # 90 deg == counter clockwise
+        # not needed as sin(90)=1 and cos(90)=0
+        # theta = radians(theta)
+        # sin_t = sin(theta)
+        # cos_t = cos(theta)
         z = cublet.z
         y = cublet.y
         cublet.z = -y
         cublet.y = z
         for sticker in cublet.stickers:
-            sticker['face'] = face_convert[sticker['face']]
+            face = sticker['face'].value
+            x, y, z = face
+            sticker['face'] = Face((x, z, -y))
         return cublet
 
     def rotate_cublet_y(self, cublet):
-        face_convert = {
-            Face.front: Face.front,
-            Face.right: Face.up,
-            Face.back: Face.back,
-            Face.left: Face.down,
-            Face.down: Face.right,
-            Face.up: Face.left,
-        }
         x = cublet.x
         z = cublet.z
         cublet.x = -z
         cublet.z = x
         for sticker in cublet.stickers:
-            sticker['face'] = face_convert[sticker['face']]
+            face = sticker['face'].value
+            x, y, z = face
+            sticker['face'] = Face((x, z, -y))
         return cublet
 
     def rotate_cublet_z(self, cublet):
-        face_convert = {
-            Face.front: Face.right,
-            Face.right: Face.back,
-            Face.back: Face.left,
-            Face.left: Face.front,
-            Face.down: Face.down,
-            Face.up: Face.up,
-        }
         x = cublet.x
         y = cublet.y
         cublet.x = -y
         cublet.y = x
         for sticker in cublet.stickers:
-            sticker['face'] = face_convert[sticker['face']]
+            face = sticker['face'].value
+            x, y, z = face
+            sticker['face'] = Face((x, z, -y))
         return cublet
 
     def rotate_face(self, face, clockwise):
@@ -120,7 +109,8 @@ class Cube:
         for column in self.cublets:
             for depth in column:
                 for cublet in depth:
-                    cublet = self.rotate_cublet_z(cublet)
+                    for i in range(n):
+                        cublet = self.rotate_cublet_z(cublet)
 
     def flip_cube(self):
         for i in range(2):
@@ -131,7 +121,15 @@ class Cube:
 
     def __init__(self):
 
-    ### X(left(red)-right(orange)) Y(front(blue)-back(green)) Z (down(yellow)-up(white))
+    # X(left(red)-right(orange)) Y(front(blue)-back(green)) Z (down(yellow)-up(white))
+        color_to_face = {
+            Color.red: Face.left,
+            Color.orange: Face.right,
+            Color.blue: Face.front,
+            Color.green: Face.back,
+            Color.yellow: Face.down,
+            Color.white: Face.up
+        }
         self.cublets = [
             [
                 ["rby", "rb", "rbw"],
@@ -157,7 +155,10 @@ class Cube:
                     for letter in self.cublets[x][y][z]:
                         for color in Color:
                             if color.name[0] == letter:
-                                stickers.append({'color': color, 'face': Face(color.value)})
+                                stickers.append(
+                                    {'color': color,
+                                     'face': color_to_face[color]}
+                                )
                                 break
                     self.cublets[x][y][z] = Cublet(x-1, y-1, z-1, stickers)
 
--- enums.py
+++ enums.py
@@ -1,11 +1,11 @@
 from enum import Enum
 class Face(Enum):
-    up = 1
-    down = 2
-    front = 3
-    back = 4
-    left = 5
-    right = 6
+    up = (0, 0, 1)
+    down = (0, 0, -1)
+    front = (0, -1, 0)
+    back = (0, 1, 0)
+    left = (-1, 0, 0)
+    right = (1, 0, 0)
 
 class Color(Enum):
     white = 1
